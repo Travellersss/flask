bb8ead64aa11b67bacc05a3a91767b7a7dc14a2a
15182620613@163.com <15182620613@163.com>
2017-12-31 20:41:05 +0800

    test2

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 #coding=utf-8
 class Config(object):
     pass
@@ -6,6 +6,4 @@ class ProdConfig(Config):
     pass
 class DecConfig(Config):
     DEBUG=True
-=======
 
->>>>>>> 6bfeca15f76034a86851aa957396d1c079c4bbc8
