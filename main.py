b570aa55a8d8932c575d0c80c6108fe2b7255238
15182620613@163.com <15182620613@163.com>
2018-01-02 20:49:05 +0800

    many to many

--- main.py
+++ main.py
@@ -15,11 +15,32 @@ class User(db.Model):
     id=db.Column(db.Integer(),primary_key=True)
     username=db.Column(db.String(255))
     password=db.Column(db.String(255))
+    posts=db.relationship('Post',backref='user',lazy='dynamic')
     def __init__(self,username):
         self.username=username
-
-
     def __repr__(self):
         return self.username
+
+
+class Post(db.Model):
+    id=db.Column(db.Integer(),primary_key=True)
+    content=db.Column(db.Text())
+    title=db.Column(db.String(255))
+    publish_date=db.Column(db.DateTime())
+    comments=db.relationship('Comment',backref='post',lazy='dylamic')
+    user_id=db.Column(db.Integer(),db.ForeignKey('user.id'))
+    def __init__(self,title):
+        self.title=title
+    def __repr__(self):
+        return self.title
+class Comment(db.Model):
+    id=db.Column(db.Integer(),primary_key=True)
+    name=db.Column(db.String(255))
+    text=db.Column(db.Text())
+    date=db.Column(db.DateTime())
+    post_id=db.Column(db.Integer(),db.ForeignKey('post.id'))
+
+    def __repr__(self):
+        return self.text[:15]
 if __name__=='__main__':
     app.run()
--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 from flask.ext.script import Manager,Server
-from main import app,User,db
+from main import app,User,db,Post
 manager=Manager(app)
 manager.add_command('server',Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app,db=db,User=User)
+    return dict(app=app,db=db,User=User,Post=Post)
 if __name__=='__main__':
     manager.run()
