19edffc6aee2c541d895871a917714499fd5ab68
15182620613@163.com <15182620613@163.com>
2018-01-07 21:05:37 +0800

    Date:1-7-21:05

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 #coding=utf-8
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,url_for,session,flash
 from config import DecConfig
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
-
-
+from flask_wtf import Form
+from wtforms import StringField,TextAreaField,PasswordField
+from wtforms.validators import DataRequired,Length,Email
+from flask_mail import Mail
+import datetime
 app=Flask(__name__)
 app.config.from_object(DecConfig)
+mail = Mail(app)
 db=SQLAlchemy(app)
+
+class UserForm(Form):
+    username = StringField('Please put your email',validators = [DataRequired(),Email()])
+    password = PasswordField('Please put your password',validators = [DataRequired(),Length(max=16)])
+class CommentForm(Form):
+    name= StringField('Name',validators = [DataRequired(),Length(max=255)])
+    text=TextAreaField(u'Comment',validators = [DataRequired()])
 
 class User(db.Model):
     id=db.Column(db.Integer(),primary_key=True)
@@ -69,14 +80,25 @@ def home(page=1):
     recent,top_tags=siderbar_data()
     return render_template('home.html',posts=posts,recent=recent,top_tags=top_tags)
 
-@app.route('/post/<int:post_id>')
+@app.route('/post/<int:post_id>',methods=['GET','POST'])
 def post(post_id):
+    form = CommentForm()
+    if form.validate_on_submit():
+        new_comment = Comment()
+        new_comment.name=form.name.data
+        new_comment.text=form.text.data
+        new_comment.post_id=post_id
+        new_comment.date = datetime.datetime.now()
+        db.session.add(new_comment)
+        db.session.commit()
+        return redirect(url_for('post',post_id=post_id))
+
     post=Post.query.get_or_404(post_id)
     tags=post.tags
-    comments=post.comments.order_by(Comments.data.desc()).all()
+    comments=post.comments.order_by(Comment.date.desc()).all()
     recent,top_tags=siderbar_data()
 
-    return render_template('post.html',post=post,tags=tags,comments=comments,recent=recent,top_tags=top_tags)
+    return render_template('post.html',post=post,tags=tags,comments=comments,form = form ,recent=recent,top_tags=top_tags)
 
 @app.route('/tag/<string:tag_name>')
 def tag(tag_name):
@@ -94,6 +116,22 @@ def user(username):
     recent,top_tags=siderbar_data()
     return render_template('user.html',user=user, posts=posts, recent=recent, top_tags=top_tags)
 
+@app.route('/login/',methods =["GET","POST"])
+def login():
+    form = UserForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username = form.username.data)
+        if user:
+            flash('already login ')
+            return redirect(url_for('login'))
+        else:
+            user = User()
+            user.username = form.username.data
+            user.password = form.password.data
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('login'))
+    return render_template('login.html',form = form )
 
 if __name__=='__main__':
     app.run()
